a1f35b7c4926c65afc3e6079b35c775d3b1aee67
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-15 15:45:35 -0300

    add formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Formulario/>
     </>
   )
 }
