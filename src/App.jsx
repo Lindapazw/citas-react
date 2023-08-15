2d38892b6e8293f1eaa5a1318466ecf0a613ad14
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-15 15:39:16 -0300

    CLEAR app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 
 function App() {
 
-  const sumar = () => {
-    console.log(1+1);
-  }
-
-  sumar();
-
-  let numero = 0;
-
-  if(numero === 0) {
-    console.log('hola');
-  }
-
-  const edad = 19;
   return (
     <>
-      {edad >= 18 ? 'eres mayor' : 'eres menor'}
-      <h1>{'Hola mundo'.toLowerCase()}</h1>
+
     </>
   )
 }
