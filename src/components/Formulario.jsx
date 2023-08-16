0223b46450393932e2aa77e33df8b704ae79f25a
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-16 14:49:24 -0300

    add handleSubmit and onChange

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,14 @@
+import { useState} from "react";
+
 const Formulario = () => {
+
+    const [nombre, setNombre] = useState('Hook');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Enviando formulario...');
+    };
+
     return (
         <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-2xl text-center">Seguimiento Pacientes</h2>
@@ -6,10 +16,11 @@ const Formulario = () => {
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="mt-5 bg-white shadow-md rounded-lg py-5 px-5">
+            <form className="mt-5 bg-white shadow-md rounded-lg py-5 px-5" onSubmit={handleSubmit}>
                 <div>
-                    <label className="font-bold" htmlFor="nombre">Nombre mascota:</label>
-                    <input className="block mt-2 border-2 w-full p-2 placeholder-gray-400 rounded-lg" type="text" placeholder="Nombre de la mascota" id="nombre"/>
+                    <label className="font-bold" htmlFor="nombre">Nombre mascota: {nombre}</label>
+                    <input className="block mt-2 border-2 w-full p-2 placeholder-gray-400 rounded-lg" type="text" placeholder="Nombre de la mascota" id="nombre" value={nombre} onChange={ (e) => setNombre(e.target.value) } 
+                    />
                 </div>
                 <div className="mt-5">
                     <label className="font-bold" htmlFor="propietario">Nombre Propietario:</label>
@@ -32,6 +43,7 @@ const Formulario = () => {
             </form>
         </div>
     )
+
 }
 
 export default Formulario
