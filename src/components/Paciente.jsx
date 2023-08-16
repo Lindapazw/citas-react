2a5ef5c8d6a80235ef6a01ebe9ed8232445f2eb8
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-16 11:27:48 -0300

    add Paciente file

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+const Paciente = () => {
+    return (
+        <div className="mt-5 bg-white shadow-md rounded-lg py-5 px-5 mx-5">
+            <p className="font-bold">Nombre:{' '}<span className="font-normal">Hook</span></p>
+            <p className="font-bold mt-3">Propietario:{' '}<span className="font-normal">Linda</span></p>
+            <p className="font-bold mt-3">Email:{' '}<span className="font-normal">Correo@corre.com</span></p>
+            <p className="font-bold mt-3">Fecha alta:{' '}<span className="font-normal">10 de Diciembre 2023</span></p>
+            <p className="font-bold mt-3">Síntomas:{' '}<span className="font-normal">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus inventore sapiente necessitatibus omnis praesentium, repellat error assumenda, eius alias, dicta ab possimus! Expedita fugiat temporibus laborum impedit vitae hic provident?</span></p>
+        </div>
+    )
+}
+
+export default Paciente
