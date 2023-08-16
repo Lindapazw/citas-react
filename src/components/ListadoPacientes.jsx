f306fbe11e64f314407dea331d534459daef51e9
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-16 11:27:36 -0300

    add ListadoPacientes styles

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,17 @@
+import Paciente from "./Paciente"
 
 const ListadoPacientes = () => {
     return (
-        <div>
-            <h2>Listado pacientes</h2>
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll mt-5 md:mt-0">
+            <h2 className="font-black text-2xl text-center">Listado pacientes</h2>
+            <p className="text-base text-center">Administra tus  {''} 
+                <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+            </p>
+
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
         </div>
     )
 }
