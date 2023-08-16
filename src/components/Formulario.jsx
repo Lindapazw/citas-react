0d252d706bce6968223c6eb64e8e80daf421dfa9
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-16 10:50:58 -0300

    add formulario styles

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,35 @@
 const Formulario = () => {
     return (
-        <div>
-            <h1>Desde Formulario</h1>
+        <div className="md:w-1/2 lg:w-2/5">
+            <h2 className="font-black text-2xl text-center">Seguimiento Pacientes</h2>
+            <p className="text-base text-center">Añade Pacientes y {''} 
+                <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
+
+            <form className="mt-10 bg-white shadow-md rounded-lg py-5 px-5">
+                <div>
+                    <label className="font-bold" htmlFor="nombre">Nombre mascota:</label>
+                    <input className="block mt-2 border-2 w-full p-2 placeholder-gray-400 rounded-lg" type="text" placeholder="Nombre de la mascota" id="nombre"/>
+                </div>
+                <div className="mt-5">
+                    <label className="font-bold" htmlFor="propietario">Nombre Propietario:</label>
+                    <input className="block mt-2 border-2 w-full p-2 placeholder-gray-400 rounded-lg" type="text" placeholder="Nombre del propietario" id="propietario"/>
+                </div>
+                <div className="mt-5">
+                    <label className="font-bold" htmlFor="email">Email:</label>
+                    <input className="block mt-2 border-2 w-full p-2 placeholder-gray-400 rounded-lg" type="email" placeholder="Contacto propietario" id="email"/>
+                </div>
+                <div className="mt-5">
+                    <label className="font-bold" htmlFor="alta">Alta:</label>
+                    <input className="block mt-2 border-2 w-full p-2 rounded-lg" type="date" id="alta"/>
+                </div>
+                <div className="mt-5">
+                    <label className="font-bold" htmlFor="sintomas">Síntomas:</label>
+                    <textarea className="block border-2 w-full rounded-lg p-2" id="sintomas" cols="30" rows="5"  placeholder="Describe los síntomas"></textarea>
+                </div>
+
+                <input className="bg-indigo-600 w-full p-2 text-white font-semibold mt-5 rounded-lg hover:bg-indigo-700 cursor-pointer transition-all" type="submit" value="Agregar paciente"/>
+            </form>
         </div>
     )
 }
