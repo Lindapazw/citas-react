41e1d809fba27797431bdb19e4e9fee1e1ece499
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-16 10:51:11 -0300

    add Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 const Header = () => {
     return (
-        <div>
-            <h1>Desde Header</h1>
-        </div>
+        <>
+            <h1 className="font-black text-2xl md:text-3xl text-center md:w-2/3 mx-auto">Seguimiento pacientes {''}
+            <span className="text-indigo-600">Veterinaria</span>
+            </h1>
+        </>
     )
 }
 
