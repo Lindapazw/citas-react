ce5c98a2bbb6c78db7e69a57a8d309ca68cde98b
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-17 12:31:38 -0300

    add ternario 'si hay pacientes' : 'No hay '

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,8 +2,10 @@ import Paciente from "./Paciente";
 import PropTypes from "prop-types";
 
 const ListadoPacientes = ({ pacientes }) => {
+
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll mt-5 md:mt-0">
+        {pacientes && pacientes.length? 'Si hay pacientes' : 'No hay pacientes'}
         <h2 className="font-black text-2xl text-center">Listado pacientes</h2>
         <p className="text-base text-center">
             Administra tus{' '}
