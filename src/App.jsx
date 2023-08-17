1b0f784f5ab3e17eac8b9712477e6bf970124d85
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-17 10:00:41 -0300

    add pacientes y setPacientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
+  const [pacientes, setPacientes] = useState([]);
+
   return (
     <div className="container mx-auto mt-12">
       <Header/>
       <div className="mt-12 md:flex">
-        <Formulario/>
+        <Formulario pacientes={pacientes} setPacientes={setPacientes} />
         <ListadoPacientes/>
       </div>
     </div>
