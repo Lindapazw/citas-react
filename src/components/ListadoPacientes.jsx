cdf799ab8ae96432f4fbe6bbb889e15142b4c10b
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-17 12:36:44 -0300

    add ternario reactivo

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -5,16 +5,27 @@ const ListadoPacientes = ({ pacientes }) => {
 
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll mt-5 md:mt-0">
-        {pacientes && pacientes.length? 'Si hay pacientes' : 'No hay pacientes'}
-        <h2 className="font-black text-2xl text-center">Listado pacientes</h2>
-        <p className="text-base text-center">
-            Administra tus{' '}
-            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-        </p>
-
-            {pacientes.map((paciente) => (
-                <Paciente key={paciente.id} paciente={paciente} />
-            ))}
+        {pacientes && pacientes.length? (
+            <>
+                <h2 className="font-black text-2xl text-center">Listado pacientes</h2>
+                <p className="text-base text-center">
+                    Administra tus{' '}
+                    <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                </p>
+        
+                    {pacientes.map((paciente) => (
+                        <Paciente key={paciente.id} paciente={paciente} />
+                    ))}
+            </>
+        ) : (
+            <>
+                <h2 className="font-black text-2xl text-center">No hay pacientes</h2>
+                <p className="text-base text-center">
+                    Comienza agregando{' '}
+                    <span className="text-indigo-600 font-bold">Pacientes</span>
+                </p>
+            </>
+        )}
 
         </div>
     );
